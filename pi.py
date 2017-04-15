32465f2ad49606a0aa7b36e2d63b6ec680309495
Daniel Kramer <drkspace@gmail.com>
2017-04-15 15:26:28 -0400

    added leibnitz

--- pi.py
+++ pi.py
@@ -1,43 +1,79 @@
 from decimal import *
 
+ZERO = Decimal('0')
+ONE = Decimal('1')
+TWO = Decimal('2')
+NEG_ONE = Decimal('-1')
+TEN = Decimal('10')
+
 class pi(object):
 
 	def __init__(self, accuracy=10):
 
-		self.accuracy=Decimal(accuracy)
-		self.pi, self.error = self.calculate()
-		
-	def calculate(self):
+		self.accuracy=accuracy
+		#self.pi, self.error = self.calculate()
+		#self.pi = self.calculate()
 
-	    # Madhava Leibniz series
-	    getcontext().prec = self.accuracy+3
-	    print self.accuracy
+	def Leibniz(self):
 
-	    #magnatude=Decimal(10)**(Decimal(-1)*(self.accuracy))	
+		getcontext().prec = self.accuracy+3
+		magnatude=TEN**(NEG_ONE*(self.accuracy+1))
+
+		sum = ZERO
+		remainder=ONE/magnatude
+		terms = (remainder+ONE)/TWO
+
+		print "Number of terms: {}".format(terms)
+
+		getcontext().prec = int(terms.log10())+2
+		print getcontext().prec
+
+		count = ONE
+
+		while count<terms+ONE: 
+			part = (NEG_ONE**(count+ONE))/(TWO*count-ONE)
+			sum += part
+			
+			count += ONE
+
+			if count % TEN**TWO == 0:
+				print "{}/{}".format(count,terms)
+
+		return (sum*Decimal('4')).quantize(magnatude)#, ROUND_HALF_UP)
+
+
+	def Madhava_Leibniz(self):
+
+	    
+	    getcontext().prec = self.accuracy+1
+
+	    #magnatude=Decimal(10)**(Decimal(-1)*(self.accuracy+1))	
+	    magnatude=Decimal(self.accuracy+1)**(Decimal(-1))
 
 	    sum = Decimal('0')
-	   # a = Decimal('12')**Decimal('.5')
+	    a = Decimal('12')**Decimal('.5')
 	    nThird = Decimal('-1')/Decimal('3')
-	    number_of_terms=self.accuracy/a
-	    number_of_terms=number_of_terms**Decimal('-1')
-	    print number_of_terms
+	    remanider=magnatude/a
+	    remanider=remanider**Decimal('-1')
+	   # print number_of_terms
 	    remainder=0
 	    num=0
-	    while(number_of_terms>=abs(remainder)):
-		num+=1
-		remainder=(Decimal('3')**Decimal(num))*Decimal((num*2)+1)
-	    	
+	    while(remanider>=remainder):
+		num+=10
+		remainder=(Decimal('3')**Decimal(num+1))*Decimal(((num+1)*2)+1)
+	    print num	
 
 	    for i in xrange(0, num, 1):
-		num = nThird**Decimal(i)
+		numerator = nThird**Decimal(i)
 		den = Decimal((i*2)+1)
-		sum = sum + (num/den)
+		sum = sum + (numerator/den)
 	       
-	   # return (a*sum).quantize(magnatude, rounding=ROUND_HALF_UP), remainder
-	    return a*sum, remainder
+	    #return (a*sum).quantize(Decimal('.1')**(Decimal(self.accuracy)), rounding=ROUND_HALF_UP), remainder**Decimal(-1)*a
+	    return (a*sum).quantize(Decimal('.00000000001'), rounding=ROUND_HALF_UP), remainder**Decimal(-1)*a
+	   # return a*sum, (remainder**Decimal(-1))*a
 	
 	def __str__(self):
-		return self.pi
+		return str(self.pi)
 		
-p=pi(accuracy = 10)
-print p
+p=pi(accuracy = 2)
+print p.Leibniz()
